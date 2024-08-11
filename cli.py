9eb7ed5dd091e5c276c7695e1d00fd864fadbf2c
Pavel Shevchuk <ps@mellifera.team>
2024-08-11 20:06:16 +0300

    Implemented per-country museum index download and caching

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import argparse
+import glob
 import json
 import math
 import os
@@ -18,6 +19,7 @@ PROJECT_ROOT = pathlib.Path(__file__).parent.resolve()
 # Define cache properties
 CACHE_ROOT = os.path.join(PROJECT_ROOT, "cache")
 COUNTRY_CACHE_MAX_AGE_MINUTES = 55
+INDEX_CACHE_MAX_AGE_HOURS = 24
 
 def load_countries():
     cache_file_path = os.path.join(CACHE_ROOT, 'homepage.html')
@@ -61,6 +63,74 @@ def load_countries():
 
     return property_list
 
+def download_country(country):
+    print(f"Downloading {selected_country['name']}...")
+
+    cache_country_root_path = os.path.join(CACHE_ROOT, 'countries')
+    if not os.path.isdir(cache_country_root_path):
+        os.mkdir(cache_country_root_path)
+
+    cache_country_path = os.path.join(cache_country_root_path, selected_country['name'])
+    if not os.path.isdir(cache_country_path):
+        os.mkdir(cache_country_path)
+
+    def download_index():
+        index_pages = []
+
+        # Delete old cache
+        for old_cache_file in glob.glob(os.path.join(cache_country_path, "[0-9]*.html")):
+            print(f"Deleting old cache file: {old_cache_file}")
+            os.remove(old_cache_file)
+
+        # Redownload country's index of museums
+        museum_list_url = f"{WEBSITE_ROOT_URL}{selected_country['relative_url']}"
+        for page_index in range(100): # make sure we never get stuck in infinite loop
+            cached_file_name = f"{page_index}.html".rjust(7, '0')
+            cached_page_path = os.path.join(cache_country_path, cached_file_name)
+            r = requests.get(museum_list_url, params={'page': page_index})
+            print(f"Downloaded {r.url}")
+            page_contents = r.text
+
+            with open(cached_page_path, "w") as f:
+                f.write(page_contents)
+
+            soup = BeautifulSoup(page_contents, 'html.parser')
+
+            index_pages.append(soup)
+
+            if not soup.find(title='Go to next page'):
+                print(f"Link to next page not found, bailing out")
+                break
+
+        return index_pages
+
+    cache_file_path = os.path.join(cache_country_path, "00.html")
+    if not os.path.isfile(cache_file_path):
+        return download_index()
+    else:
+        cache_file_modification_timestamp = os.path.getmtime(cache_file_path)
+        current_timestamp = time.time()
+        cache_file_age_seconds = current_timestamp - cache_file_modification_timestamp
+        cache_file_age_hours = math.floor(cache_file_age_seconds / 60 / 60)
+        print(f"{selected_country['name']} index cache is {cache_file_age_hours} hours old")
+
+        if cache_file_age_hours < INDEX_CACHE_MAX_AGE_HOURS:
+            print("Loading cached index...")
+            index_pages = []
+            
+            sorted_cache_file_path_array = sorted(glob.glob(os.path.join(cache_country_path, "[0-9]*.html")))
+            for cache_file_path in sorted_cache_file_path_array:
+                print(f"Loading contents of {cache_file_path}...")
+                with open(cache_file_path, 'r') as f:
+                    html_contents = f.read()
+                    soup = BeautifulSoup(html_contents, 'html.parser')
+
+                    index_pages.append(soup)
+            
+            return index_pages
+        else:
+            return download_index()
+
 # Ensure cache_root exists
 if not os.path.isdir(CACHE_ROOT):
     os.mkdir(CACHE_ROOT)
@@ -75,13 +145,15 @@ arg_parser = argparse.ArgumentParser(epilog=f"Available countries: {readable_cou
 arg_parser.add_argument('--country', help='Limit scrape to one country')
 args = arg_parser.parse_args()
 
+country_indexes = []
+
 if args.country:
     country_search_results = list(filter(lambda c: c['name'] == args.country, countries))
     if len(country_search_results) < 1:
         sys.exit(f"Country \"{args.country}\" not found.\n\nTry any of these: {readable_country_list}")
 
     selected_country = country_search_results[0]
-    print(f"Downloading {selected_country}...")
+    country_indexes.append(download_country(selected_country))
 else:
-    for country in countries:
-        print(f"Downloading {country['name']}...")
+    for selected_country in countries:
+        country_indexes.append(download_country(selected_country))
